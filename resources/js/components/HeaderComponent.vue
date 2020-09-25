9f191984bb557c464a08fb0d90a5cab3efac1a97
宜志富紹太 <shotagishitomi@gmail.com>
2020-09-25 23:14:38 +0900

    新規作成初vue.js + laravelでのアプリ開発 2020/09/25

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-fluid bg-dark">
+    <div class="container">
+      <nav class="navbar navbar-dark bg-dark header">
+        <!-- Navbar content -->
+        <p class="navbar-brand my-2">Vue Laravel SPA</p>
+        <div class="form-inline my-2 my-lg-0">
+            <button class="btn btn-outline-primary my-2 my-sm-0 mx-3">
+                List
+            </button>
+          <button class="btn btn-outline-success mx-3 my-2 my-sm-0">
+            ADD
+          </button>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+// 他のコンポーネントには適用されない！！
+<style scoped>
+.header {
+  color: white;
+}
+</style>
+
+<script>
+export default {};
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{config('app.name', 'Vue Laravel SPA')}}</title>
+    <!-- resources/css/app.cssにアクセス -->
+    <!-- 最初からBootStrapが入っている -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<div id="app">
+    <header-component></header-component>
+    
+</div>
+
+<!-- 最初からVue.jsが入っている。 -->
+<script src="{{mix('/js/app.js')}}" defer></script>
+</body>
+
+</html>
