5987d74e5c82dc3ddaa577ee94b514026a2cb965
宜志富紹太 <shotagishitomi@gmail.com>
2020-09-28 02:25:38 +0900

    no message

---
+++ database/seeders/TaskTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+use App\Task;
+use Illuminate\Database\Seeder;
+
+class TaskTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            Task::create([
+                'title' => 'title' . $i,
+                'content' => 'content' . $i,
+                'person' => 'person' . $i,
+            ]);
+        }
+    }
+}
